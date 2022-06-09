380caa196a1d5b04278cc47fcd412bfd481f0a4d
Charles <achigbulamcharles49@web.de>
2022-06-09 00:42:30 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * except 2 and 4, followed by a new line
+ */
+void print_most_numbers(void)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
