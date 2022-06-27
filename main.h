a0dfdda2ef4762e14c466f93d3552fc430c01908
Charles <achigbulamcharles49@web.de>
2022-06-27 03:31:15 -0700

    main.h

--- main.h
+++ main.h
@@ -1,17 +1,21 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-int _putchar(char c);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_strdup(char *str);
+
+char **strtow(char *str);
+
+char *argstostr(int ac, char **av);
+
+char *str_concat(char *s1, char *s2);
+
+void free_grid(int **grid, int height);
+
+int **alloc_grid(int width, int height);
+
+char *create_array(unsigned int size, char c);
 
 #endif
