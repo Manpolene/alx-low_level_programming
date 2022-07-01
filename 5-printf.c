90c8644a39a359af4ee8a4d798c8aae990ffbd11
Charles <achigbulamcharles49@web.de>
2022-07-01 06:10:52 -0700

    5-printf.c

---
+++ 5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - Entry point
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper gramar, but the outcome is a piece of art,\n");
+	return (0);
+}
